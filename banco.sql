b0d62cf94fa043a8b8758695ff14cfb02b432c15
Arthur-Bamberg <bamberguisses@gmail.com>
2022-11-09 15:18:06 -0300

    banco criado com sql testado

--- banco.sql
+++ banco.sql
@@ -1,37 +1,37 @@
 create table client (
     idClient number primary key,
-    name varchar(50) not null,
-    email varchar(50) not null,
-    password varchar(50) not null
+    name varchar2(50) not null,
+    email varchar2(50) not null,
+    password varchar2(50) not null
 );
 
 create table reit (
     idReit number primary key,
-    code varchar(6) not null,
-    name varchar(50),
-    section varchar(20),
-    subsection varchar(20),
-    mainIndex varchar(5)
+    code varchar2(6) not null,
+    name varchar2(50),
+    section varchar2(20),
+    subsection varchar2(20),
+    mainIndex varchar2(5)
 );
 
 create table stock (
     idStock number primary key,
-    code varchar(6) not null,
-    name varchar(50),
-    section varchar(20),
-    subsection varchar(20),
-    mainIndex varchar(5)
+    code varchar2(6) not null,
+    name varchar2(50),
+    section varchar2(20),
+    subsection varchar2(20),
+    mainIndex varchar2(5)
 );
 
 create table clientReit (
     FK_idClient number,
     FK_idReit number,
     quantity number,
-    objective varchar(100),
-    risk varchar(20),
-    fundamentals varchar(100),
-    strategy varchar(100),
-    caution varchar(100),
+    objective varchar2(100),
+    risk varchar2(20),
+    fundamentals varchar2(100),
+    strategy varchar2(100),
+    caution varchar2(100),
     foreign key (FK_idClient) references client(idClient),
     foreign key (FK_idReit) references reit(idReit),
     primary key (FK_idClient, FK_idReit)
@@ -41,11 +41,11 @@ create table clientStock (
     FK_idClient number,
     FK_idStock number,
     quantity number,
-    objective varchar(100),
-    risk varchar(20),
-    fundamentals varchar(100),
-    strategy varchar(100),
-    caution varchar(100),
+    objective varchar2(100),
+    risk varchar2(20),
+    fundamentals varchar2(100),
+    strategy varchar2(100),
+    caution varchar2(100),
     foreign key (FK_idClient) references client(idClient),
     foreign key (FK_idStock) references Stock(idStock),
     primary key (FK_idClient, FK_idStock)
@@ -55,7 +55,7 @@ create table reitAcquisition (
     idReitAcquisition number primary key,
     FK_idReit number,
     FK_idClient number,
-    date date,
+    aDate date,
     quantity number,
     price number,
     foreign key (FK_idReit) references reit(idReit),
@@ -66,7 +66,7 @@ create table stockAcquisition (
     idStockAcquisition number primary key,
     FK_idStock number,
     FK_idClient number,
-    date date,
+    aDate date,
     quantity number,
     price number,
     foreign key (FK_idStock) references stock(idStock),
@@ -79,7 +79,7 @@ create table keepAnEyeOnReit (
     lastDaySeen date,
     price number,
     fairPrice number,
-    reason varchar(100),
+    reason varchar2(100),
     foreign key (FK_idClient) references client(idClient),
     foreign key (FK_idReit) references reit(idReit),
     primary key (FK_idClient, FK_idReit)
@@ -89,10 +89,10 @@ create table toRentStock (
     idToRentStock number primary key,
     FK_idStock number,
     FK_idClient number,
-    date date,
+    aDate date,
     quantity number,
     fee number,
-    isActive boolean,
+    isActive number,
     foreign key (FK_idStock) references stock(idStock),
     foreign key (FK_idClient) references client(idClient)
 );
@@ -105,7 +105,7 @@ create table rentedStock (
     dateEnd date,
     quantity number,
     fee number,
-    isActive boolean,
+    isActive number,
     foreign key (FK_idStock) references stock(idStock),
     foreign key (FK_idClient) references client(idClient)
 );
@@ -114,10 +114,10 @@ create table toRentReit (
     idToRentReit number primary key,
     FK_idReit number,
     FK_idClient number,
-    date date,
+    aDate date,
     quantity number,
     fee number,
-    isActive boolean,
+    isActive number,
     foreign key (FK_idReit) references reit(idReit),
     foreign key (FK_idClient) references client(idClient)
 );
@@ -130,7 +130,7 @@ create table rentedReit (
     dateEnd date,
     quantity number,
     fee number,
-    isActive boolean,
+    isActive number,
     foreign key (FK_idReit) references reit(idReit),
     foreign key (FK_idClient) references client(idClient)
 );
